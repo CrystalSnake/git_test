93e838b0df2846d732915c05a9a51c6fa7c67e0c
Crystal Snake <22963880+CrystalSnake@users.noreply.github.com>
2022-12-25 13:15:15 +0400

    Asset Management - Wrapping Up

--- src/index.js
+++ src/index.js
@@ -1,20 +1,4 @@
-import './style.css';
-import Icon from './gear.png';
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
-// import _ from 'lodash';
+import _ from 'lodash';
 
 // function component() {
 //   const element = document.createElement('div');
@@ -36,16 +20,6 @@ function component() {
 
   // use your function!
   element.textContent = myName('Cody');
-  element.classList.add('hello');
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  console.log(Data);
-  console.log(Notes);
 
   return element;
 }
